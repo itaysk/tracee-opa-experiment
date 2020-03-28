5d97e5d485a5ea3708cb86bc76631e6c75bc9d38
Itay Shakury <itay@itaysk.com>
2020-03-28 16:55:42 +0300

    wip

--- detect/tracee-opa-sm.sh
+++ detect/tracee-opa-sm.sh
@@ -1,22 +1,25 @@
 #! /bin/bash 
-
+set -x
 rego="$1"
 [ -f "$rego" ] || exit 1
-state_id="$(basename "$0" .sh)_$RANDOM"
+state_id="$(basename "$0" .sh)" #_$RANDOM"
 state_path="./state/$state_id"
 mkdir -p "$state_path"
-state_file="$state_path/state.json"
-newstate_file="$state_path/newstate.json"
-[ -f "$state_file" ] || echo '{}' > "$state_file"
+state_file="$state_path/state"
+rm "$state_file" && mkfifo "$state_file" 
+#exec 3<>"$state_file"
+echo '{}' >$state_file &
+cat $state_file
+#exec 3<>&-
 
+exit
 while read -r line; do
   opa eval \
     --format bindings --fail \
-    --data "$state_file" \
+    --data <(cat - <&3) \
     --data "$rego" \
     'state = data.example' \
     --stdin-input <<< "$line" \
-  > "$newstate_file" 
-  mv "$newstate_file" "$state_file"
-  jq '.state.detected != true' "$state_file" -e >/dev/null || jq -c -M '.' "$state_file"
+  | jq '.'
+  #| jq 'if .state.detected == true then . else "" end' -c -M
 done </dev/stdin
