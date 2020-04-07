7b534c295de4a6e8ada2001b76a68a7c8408f0ee
Itay Shakury <itay@itaysk.com>
2020-04-07 16:27:54 +0000

    use fifo instead of file

--- detect/tracee-opa-sm.sh
+++ detect/tracee-opa-sm.sh
@@ -6,17 +6,21 @@ state_id="$(basename "$0" .sh)_$RANDOM"
 state_path="./state/$state_id"
 mkdir -p "$state_path"
 state_file="$state_path/state.json"
-newstate_file="$state_path/newstate.json"
-[ -f "$state_file" ] || echo '{}' > "$state_file"
 
+rm "$state_file" &>/dev/null; mkfifo "$state_file"
+exec 3<>"$state_file"
+echo '{}' > "$state_file" &
 while read -r line; do
+  read -r state <"$state_file"
   opa eval \
     --format bindings --fail \
-    --data "$state_file" \
+    --data <(echo $state) \ #TODO: why can't give $state_file directly here?
     --data "$rego" \
     'state = data.example' \
     --stdin-input <<< "$line" \
-  > "$newstate_file" 
-  mv "$newstate_file" "$state_file"
-  jq '.state.detected != true' "$state_file" -e >/dev/null || jq -c -M '.' "$state_file"
-done </dev/stdin
+  | jq '.' -cM \
+  | tee >&3 \
+      >(jq 'select(.state.detected == true)')
+done 3>$state_file </dev/stdin 
+
+exec 3>&-
